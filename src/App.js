d811dc079bad8d804872eb1453c60974ad30d0fc
saranya-302 <158792493+saranya-302@users.noreply.github.com>
2024-05-24 06:56:26 +0000

    front end work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(""); 
+  useEffect(()=> {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("https://simple-front.vercel.app/", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        setValue(result);
+      })
+      .catch((error) => console.error(error));
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
-       my name is saranya
+        my name is saranya
+        <p>
+          { value }
+        </p>
       </header>
     </div>
   );
